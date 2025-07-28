efaf15e92ee8142e098d9978b84f60b893fd538f
bruna2024 <159148692+bruna2024@users.noreply.github.com>
2025-07-28 07:59:05 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -8,29 +8,32 @@ const perguntas = [
     {
         enunciado:'pergunta 1',
         alternativas: [
-            "alernativa 1",
-            "alternativa 2"
-        ]
+            {
+            "alernativas 1",
+            "alternativas 2",
     },
     {
+    ]
+
+    {}
         enunciado:'pergunta 2',
         alternativas: [
-            "alernativa 3",
-            "alternativa 4"
+            "alernativas 3",
+            "alternativas 4"
         ]
     },
     {
         enunciado:'pergunta 3',
         alternativas: [
-            "alernativa 5",
-            "alternativa 6"
+            "alernativas 5",
+            "alternativas 6"
         ]
     },
     {
         enunciado:'pergunta 4',
         alternativas: [
-            "alernativa 7",
-            "alternativa 8"
+            "alernativas 7",
+            "alternativas 8"
         ]
     }
 ]
@@ -42,20 +45,29 @@ let hitoriaFinal = " ";
 function mostraperguntas(){
     perguntaatual = perguntas[atual]
     caixaperguntas.textContent = perguntaatual.enunciado
-    mostrarAlternativa ();
+    mostrarAlternativas ();
 }
 
-function mostrarAlternativa(){
-    for( const alternativa of perguntaatual.alternativas ){
+function mostrarAlternativas(){
+    if(atual >= perguntas.length)
+
+    for( const alternativas of perguntaatual.alternativas ){
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa))
+        botaoAlternativas.textContent = alternativas.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativas))
         caixaAlternativas.appendChild(botaoAlternativas)
     }
 }
 function respostaSelecionada(opcaoSelecionada){
+    const afirmacao = opcao
     atual++
     mostraperguntas();
+}
+
+function mostreResultado (){
+    caixaperguntas.textContent = "";
+    textoResultado.textContent = historiaFinal;
+    caixaalternativas.textContent = ";"
 }
 
 mostraperguntas();
